2921a1eb2c3fd13c78f38ae3f5f83abb01cc71ec
kfairrytail <167627897+kfairrytail@users.noreply.github.com>
2024-04-25 09:12:14 +0700

    first commit

---
+++ application/views/user/index.php
@@ -0,0 +1,7 @@
+<br><br><br>
+    <div class="container text-center" style="margin: 2em auto;">
+      <div class="jumbotron">
+        <h1 class="display-3"></h1>
+        <p class="lead"></p>
+    </div>
+  </div>
